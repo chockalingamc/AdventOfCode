cdc0537a8e2bc50d0cb693709cf494c09d40185f
Chocka Chidambaram <7660279+chockalingamc@users.noreply.github.com>
2025-12-02 18:48:33 -0500

    Day 2 Part 2

--- 2025-python/src/day2_gift_shop.py
+++ 2025-python/src/day2_gift_shop.py
@@ -8,18 +8,37 @@ id_ranges = [id_range for line in lines for id_range in line.split(",")]
 start_time = datetime.now()
 print(f"Start time: {start_time}\n")
 
-total = 0
+first_total = 0
+second_total = 0
 for id_range in id_ranges:
     [start, end] = id_range.split("-")
     for id in range(int(start), int(end) + 1):
         id_str = str(id)
-        mid = len(id_str) // 2
-        first_half = id_str[:mid]
-        second_half = id_str[mid:]
-        if first_half == second_half:
-            total += id
+        length = len(id_str)
+        mid = length // 2
+        for chunk_size in range(mid, 0, -1):
+            if length % chunk_size: # string does not split into equal part of chunk_size length
+                continue
+            first_chunk = id_str[:chunk_size]
+            is_invalid = True
+            for i in range(1, length // chunk_size):
+                next_chunk = id_str[chunk_size*i:chunk_size*(i+1)]
+                if first_chunk != next_chunk:
+                    is_invalid = False
+                    break
+            if is_invalid:
+                if (chunk_size << 1) == length:
+                    first_total += id
+                second_total += id
+                break
+
+        # first_half = id_str[:mid]
+        # second_half = id_str[mid:]
+        # if first_half == second_half:
+        #    first_total += id
 
 end_time = datetime.now()
-print(f"Part 1 Answer: {total}")
+print(f"Part 1 Answer: {first_total}")
+print(f"Part 2 Answer: {second_total}")
 print(f"\nEnd time: {end_time}")
 print(f"Time taken: {end_time - start_time}\n")
